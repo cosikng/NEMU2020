851098a752b7ae0365c30cfecb415f03e3f73ef6
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 15:19:04 +0800

    >  compile NEMU 20192160000 abc Linux ubuntu 5.4.0-38-generic #42~18.04.1-Ubuntu SMP Mon Jun 15 09:48:10 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:19:04 up 10:14,  1 user,  load average: 0.40, 0.25, 0.20 dc59e560f30e0f582342e6d1f6cbfa998c075621

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -156,7 +156,7 @@ uint32_t expr(char *e, bool *success)
 
 	int i;
 	for(i=0;i<nr_token;i++){
-		if(tokens[nr_token].type==0) printf("%d\t%s\n",tokens[i].type,tokens[i].str);
+		if(tokens[i].type==0) printf("%d\t%s\n",tokens[i].type,tokens[i].str);
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
