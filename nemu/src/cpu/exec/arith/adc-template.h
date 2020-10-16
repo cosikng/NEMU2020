408aedf9ad6fdaf981a48ebb4bf3814fa408bd5b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-16 10:40:56 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:40:56 up 12:07,  1 user,  load average: 0.05, 0.13, 0.21 c05e0e8eccf351089c8c799cc7da643763701e08

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,36 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+
+
+static void do_execute()
+{
+    DATA_TYPE result = op_dest->val + op_src->val + cpu.eflags.CF;
+    int l = (DATA_BYTE << 3) - 1;
+    cpu.eflags.CF = (result < op_dest->val);
+    cpu.eflags.SF = result >> l;
+    int s1, s2;
+    s1 = op_dest->val >> l;
+    s2 = op_src->val >> l;
+    cpu.eflags.OF = (s1 == s2 && s1 != cpu.eflags.SF);
+    cpu.eflags.ZF = !result;
+    result ^= result >> 4;
+    result ^= result >> 2;
+    result ^= result >> 1;
+    cpu.eflags.PF = !(result & 1);
+    print_asm_template2_n();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+
+
+#include "cpu/exec/template-end.h"
