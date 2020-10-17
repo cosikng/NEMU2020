d691735abf20d45920ab2e9ff99e296b675e8a6b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-17 15:17:49 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:17:49 up 1 day,  8:00,  1 user,  load average: 1.04, 0.56, 0.27 2de495e3724c0f91f4e57204fa345ab7e91eeeda

---
+++ nemu/src/cpu/exec/call/jno.h
@@ -0,0 +1,11 @@
+#include "cpu/helper.h"
+#ifndef __JNO_H__
+#define __JNO_H__
+
+make_helper(jno_si_b);
+make_helper(jno_si_l);
+make_helper(jno_si_w);
+make_helper(jno_si_v);
+
+
+#endif
---
+++ nemu/src/cpu/exec/call/jns.h
@@ -0,0 +1,11 @@
+#include "cpu/helper.h"
+#ifndef __JNS_H__
+#define __JNS_H__
+
+make_helper(jns_si_b);
+make_helper(jns_si_l);
+make_helper(jns_si_w);
+make_helper(jns_si_v);
+
+
+#endif
