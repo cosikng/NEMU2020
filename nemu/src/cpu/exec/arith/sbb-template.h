c4483dcbbebc1aed7123e8175086f951eb8b1e2a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 16:01:00 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:01:00 up 1 day, 21:15,  1 user,  load average: 0.23, 0.15, 0.17 f02855763b30b8086f89bebae0383a28887b50e4

---
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -0,0 +1,39 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sbb
+
+static void do_execute()
+{
+    DATA_TYPE src = op_src->val;
+    if (op_src->size == 1 && op_dest->size != 1)
+    {
+        op_src->val = (int8_t)op_src->val;
+    }
+    src += cpu.eflags.CF;
+    DATA_TYPE result = op_dest->val - src;
+    int l = (DATA_BYTE << 3) - 1;
+    cpu.eflags.CF = op_dest->val < src;
+    cpu.eflags.SF = result >> l;
+    int s1, s2;
+    s1 = op_dest->val >> l;
+    s2 = src >> l;
+    cpu.eflags.OF = (s1 != s2 && s2 == cpu.eflags.SF);
+    cpu.eflags.ZF = !result;
+    OPERAND_W(op_dest, result);
+    result ^= result >> 4;
+    result ^= result >> 2;
+    result ^= result >> 1;
+    cpu.eflags.PF = !(result & 1);
+    print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+                make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
