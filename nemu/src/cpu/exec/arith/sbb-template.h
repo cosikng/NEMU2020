48c1dd839fcf4e655b486d8244f4087490b922ba
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 16:05:42 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:05:42 up 1 day, 21:20,  1 user,  load average: 0.42, 0.22, 0.18 9401b0e5ca6f7df5c2770f613e4c737b0438d95

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -33,7 +33,7 @@ make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-                make_instr_helper(si2rm)
+make_instr_helper(si2rm)
 #endif
 
 #include "cpu/exec/template-end.h"
