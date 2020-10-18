cd356544430b0c8477b8a59f8b90e7de2047284d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-18 15:37:49 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:37:49 up 1 day, 20:52,  1 user,  load average: 0.50, 0.24, 0.21 2b9a9e0fa9ce7d9e6d54b4a96b3c49a630ed71a0

---
+++ nemu/src/cpu/exec/data-mov/cbw.c
@@ -0,0 +1,11 @@
+#include "cpu/exec/helper.h"
+
+make_helper(cbw){
+    if(ops_decoded.is_operand_size_16){
+        if((cpu.gpr[0]._8[0]&0x80)!=0) cpu.gpr[0]._16|=0xff00;
+    }
+    else{
+        if((cpu.gpr[0]._16&0x8000)!=0) cpu.gpr[0]._32|=0xffff0000;
+    }
+    return 1;
+}
---
+++ nemu/src/cpu/exec/data-mov/cbw.h
@@ -0,0 +1,8 @@
+#ifndef __CBW_H__
+#define __CBW_H__
+
+#include "cpu/exec/helper.h"
+
+make_helper(cbw);
+
+#endif
