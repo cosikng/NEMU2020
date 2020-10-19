53598e4896269f78da201e3f5cb09aeec055fa14
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-19 16:43:51 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:43:51 up 2 days, 21:00,  1 user,  load average: 0.66, 0.22, 0.27 cd4b53282117bd5c533961a77735cd77c421c43d

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -15,7 +15,7 @@ make_helper(rep) {
 			exec(eip + 1);
 			count ++;
 			cpu.ecx --;
-			assert(ops_decoded.opcode == 0xa4	// movsb
+			/*assert(ops_decoded.opcode == 0xa4	// movsb
 				|| ops_decoded.opcode == 0xa5	// movsw
 				|| ops_decoded.opcode == 0xaa	// stosb
 				|| ops_decoded.opcode == 0xab	// stosw
@@ -23,13 +23,18 @@ make_helper(rep) {
 				|| ops_decoded.opcode == 0xa7	// cmpsw
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
-				);
+				);*/
+			if((ops_decoded.opcode == 0xa6
+				|| ops_decoded.opcode == 0xa7
+				|| ops_decoded.opcode == 0xae
+				|| ops_decoded.opcode == 0xaf) && cpu.eflags.ZF == 0) break;
 
 			/* TODO: Jump out of the while loop if necessary. */
 
 		}
 		len = 1;
 	}
+	print_asm_template_s();
 
 #ifdef DEBUG
 	char temp[80];
