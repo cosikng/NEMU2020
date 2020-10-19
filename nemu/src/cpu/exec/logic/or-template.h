5d522264f028616793a3350433ae511b8d63fe96
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-19 10:57:34 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:57:34 up 2 days, 15:59,  1 user,  load average: 0.50, 0.63, 0.54 1791c6b435dc8a4315315e038dbfceae446872ca

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -4,10 +4,18 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val | op_src->val;
+	int l = (DATA_BYTE << 3) - 1;
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
+	cpu.eflags.SF = result >> l;
+	cpu.eflags.ZF = !result;
 	OPERAND_W(op_dest, result);
-
+	result ^= result >> 4;
+	result ^= result >> 2;
+	result ^= result >> 1;
+	cpu.eflags.PF = !(result & 1);
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	//panic("please implement me");
 
 	print_asm_template2();
 }
