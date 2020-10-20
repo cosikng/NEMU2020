daee7639ee78351ae90c433538f43400e7dcff14
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 10:57:56 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:57:55 up  2:23,  1 user,  load average: 0.49, 0.28, 0.27 f9ada0dfdc6671ece47d7f2efcf8b44c4b2d69ff

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -51,11 +51,11 @@ make_helper(repnz) {
 		exec(eip + 1);
 		count ++;
 		cpu.ecx --;
-		assert(ops_decoded.opcode == 0xa6	// cmpsb
-				|| ops_decoded.opcode == 0xa7	// cmpsw
-				|| ops_decoded.opcode == 0xae	// scasb
-				|| ops_decoded.opcode == 0xaf	// scasw
-			  );
+		if((ops_decoded.opcode == 0xa6
+				|| ops_decoded.opcode == 0xa7
+				|| ops_decoded.opcode == 0xae
+				|| ops_decoded.opcode == 0xaf) && cpu.eflags.ZF == 1) break;
+
 
 		/* TODO: Jump out of the while loop if necessary. */
 
