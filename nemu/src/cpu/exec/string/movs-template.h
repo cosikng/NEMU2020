b6e3601ac952e2e80ce91acd0b9eb47777988e74
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 17:20:26 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:20:26 up  8:15,  1 user,  load average: 0.34, 0.31, 0.25 3905e041ed22966652f10d049b7dd0a07f4eaeb8

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -1,13 +1,17 @@
 #include "cpu/exec/template-start.h"
 
 #define instr movs
+
 extern uint8_t *hw_mem;
 
 static void do_execute()
 {
-	uint32_t x=(uint32_t)hw_mem;
-	printf("\n0x%x\n",x);
-	swaddr_write(cpu.edi, 4, swaddr_read(cpu.esi, 4));
+	
+	//swaddr_write(cpu.edi, 4, swaddr_read(cpu.esi, 4));
+	int i;
+	for(i=0;i<3;i++){
+		*(hw_mem+cpu.edi+i)=*(hw_mem+cpu.esi+i);
+	}
 	if (cpu.eflags.DF == 0)
 	{
 		cpu.edi += DATA_BYTE;
