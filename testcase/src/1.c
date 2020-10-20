d6ad59581ae8a68eafdc5d6701fa8e3b3a077ca5
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 11:32:45 +0800

    >  run 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:32:45 up  2:58,  1 user,  load average: 0.12, 0.26, 0.26 e5aec66bb1dfb71970e20a581c275f3b8ed0cbb2

--- testcase/src/1.c
+++ testcase/src/1.c
@@ -1,10 +1,11 @@
 #include "trap.h"
+#include <stdio.h>
 #include <string.h>
 
 int main() {
 	char s[]="Hello";
 	char a[10];
-	strcpy(a,s);
+	sprintf(a,"%s",s);
 
 	nemu_assert(strcmp(a,"Hello")==0);
 	return 0;
