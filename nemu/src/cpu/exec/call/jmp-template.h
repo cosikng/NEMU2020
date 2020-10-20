a1e74c8895e5a971ca4faaea3a24a05e2b1713bb
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 16:31:19 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:31:19 up  7:25,  1 user,  load average: 0.82, 0.47, 0.41 99eb7974f1fab55842f438b18af3a8dfa95ade86

--- nemu/src/cpu/exec/call/jmp-template.h
+++ nemu/src/cpu/exec/call/jmp-template.h
@@ -4,15 +4,27 @@
 
 static void do_execute()
 {
-    int off = op_src->val;
-    cpu.eip += off;
+    if (op_src->type == OP_TYPE_IMM)
+    {
+        int off = op_src->val;
+        cpu.eip += off;
+    }
+    else
+    {
+        cpu.eip = op_src->val;
+        if (DATA_BYTE == 2)
+        {
+            cpu.eip &= 0xffff;
+        }
+        cpu.eip -= 2;
+    }
     print_asm_template1_n();
     return;
 }
 
 make_instr_helper(si)
-/*#if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(rm)
-#endif*/
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+    make_instr_helper(rm)
+#endif
 
 #include "cpu/exec/template-end.h"
