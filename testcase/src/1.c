8e1bd752f28e60bdfc23dcf74d27cdfb7efef558
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 19:47:05 +0800

    >  run 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:47:05 up 12:40,  1 user,  load average: 0.24, 0.20, 0.18 8f3d4d97f9a7c313c38fd8f65b390963722a4f

--- testcase/src/1.c
+++ testcase/src/1.c
@@ -14,8 +14,8 @@ void f(char *a,char *b){
 int main() {
 	char s[]="abcde";
 	char a[10];
-	//memcpy(a,s,6);
-	snprintf(a,6,"%s",s);
+	memcpy(a,s,6);
+	//snprintf(a,6,"%s",s);
 	nemu_assert(strcmp(a,s)==0);
 	return 0;
 }
