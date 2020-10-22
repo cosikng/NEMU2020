89ceaa399e0fae372662a1819e7fa97b12725e12
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 15:55:28 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:55:28 up 18:59,  1 user,  load average: 0.13, 0.13, 0.09 e7a6152315f9d72a521b902dd2e73c955c6863d8

--- testcase/src/1.c
+++ testcase/src/1.c
@@ -1,5 +1,5 @@
 #include "trap.h"
-#include <string.h>
+/*#include <string.h>
 #include <stdio.h>
 
 void f(char *a,char *b){
@@ -9,13 +9,17 @@ void f(char *a,char *b){
 	}
 	a[i]=0;
 	return;
-}
+}*/
 
 int main() {
-	char s[]="ab";
+	/*char s[]="ab";
 	char a[10];
 	memcpy(a,s,3);
 	//snprintf(a,3,"%s",s);
-	nemu_assert(strcmp(a,s)==0);
+	nemu_assert(strcmp(a,s)==0);*/
+	int a=-2;
+	unsigned int b=-2;
+	nemu_assert((a>>1)==-1);
+	nemu_assert((b>>1)==2147483647);
 	return 0;
 }
