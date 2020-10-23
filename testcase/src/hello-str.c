7fd64b2c125820e9a214787f173995a827944286
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-23 23:02:57 +0800

    >  run 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:02:57 up 1 day,  3:02,  1 user,  load average: 0.79, 0.25, 0.14 534a06c278fc67e333e7892ce1aeeb7eef214eb1

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -5,7 +5,7 @@
 char buf[128];
 
 int main() {
-	sprintf(buf, "%s", "Hello world!\n");
+	sprintf(buf, "Hello world!\n");
 	//strcpy(buf,"Hello world!\n");
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
