30fc807a507929a56af7f3d4af058958e5bdc627
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-24 23:00:24 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:00:24 up 1 day, 14:32,  1 user,  load average: 0.13, 0.08, 0.02 d1f92eb3dd3f611056414a8c81c8230344793ac1

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -3,21 +3,17 @@
 #include "trap.h"
 
 char buf[128];
-char s[]="a";
-char format[]="%s";
 
 int main() {
-	sprintf(buf,format,s);
-	nemu_assert(strcmp(buf, "a") == 0);
 
-	//sprintf(buf, "%s", "Hello world!\n");
-	//nemu_assert(strcmp(buf, "Hello world!\n") == 0);
+	sprintf(buf, "%s", "Hello world!\n");
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	/*sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
-	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);*/
+	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	return 0;
 }
