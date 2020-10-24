0510964d58620dbe412aed80c63e3d7bcd59f21c
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-24 13:59:02 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:59:02 up 1 day,  7:02,  1 user,  load average: 0.12, 0.18, 0.17 6c55d8668969d7ae3e116e40c47a6fca969387

--- nemu/src/cpu/exec/push/leave.c
+++ nemu/src/cpu/exec/push/leave.c
@@ -4,15 +4,15 @@
 
 make_helper(leave)
 {
+    cpu.esp = cpu.ebp;
     if (ops_decoded.is_operand_size_16)
     {
-        cpu.gpr[4]._16 = cpu.gpr[5]._16;
         cpu.gpr[5]._16 = swaddr_read(cpu.gpr[4]._16, 2);
         cpu.gpr[4]._16 += 2;
     }
     else
     {
-        cpu.esp = cpu.ebp;
+        
         cpu.ebp = swaddr_read(cpu.esp, 4);
         cpu.esp += 4;
     }
