ee8003ad37d65be606fd21aa6659898e8b6831ae
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-24 20:42:57 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-51-generic #56~18.04.1-Ubuntu SMP Tue Oct 6 09:47:18 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:42:57 up 1 day, 13:12,  1 user,  load average: 0.02, 0.07, 0.08 731ba67abc27e062619ecce490abeebd69c7833

--- nemu/src/cpu/exec/push/push-template.h
+++ nemu/src/cpu/exec/push/push-template.h
@@ -4,22 +4,23 @@
 
 static void do_execute()
 {
-	int byte;
+	int byte, data;
 	if (op_src->type == OP_TYPE_IMM)
 		byte = 4;
 	else
 		byte = DATA_BYTE;
 	cpu.esp -= byte;
-	swaddr_write(cpu.esp, byte, swaddr_read(op_src->addr - byte,byte));
+	data = op_src->val;
+	swaddr_write(cpu.esp, byte, data);
 	print_asm_template1_n();
 	return;
 }
 
 make_instr_helper(rm)
-make_instr_helper(i)
-make_instr_helper(si)
+	make_instr_helper(i)
+		make_instr_helper(si)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(r)
+			make_instr_helper(r)
 #endif
 
 #include "cpu/exec/template-end.h"
