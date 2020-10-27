daf86c55618a20b8baadb8eac8979f6f4873bbfe
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-27 09:50:14 +0800

    >  compile kernel 3019244336 abc Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:50:14 up 11:26,  1 user,  load average: 0.80, 0.35, 0.26 5784d2847fe4f124f7c6185444566014b3e1c536

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -16,7 +16,8 @@ void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 void create_video_mapping();
 uint32_t get_ucr3();
 
-uint32_t loader() {
+uint32_t loader()
+{
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
@@ -28,7 +29,7 @@ uint32_t loader() {
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
 
-	elf = (void*)buf;
+	elf = (void *)buf;
 
 	/* TODO: fix the magic number with the correct one */
 	//nemu_assert(0);
@@ -37,26 +38,34 @@ uint32_t loader() {
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+	ph = (void *)(buf + elf->e_phoff);
+	int i;
+	for (i = 0; i < elf->e_phnum; i++, ph++)
+	{
 		/* Scan the program header table, load each segment into memory */
-		if(ph->p_type == PT_LOAD) {
+		if (ph->p_type == PT_LOAD)
+		{
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
-			 
+
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 
+			ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
+			if (cur_brk < new_brk)
+			{
+				max_brk = cur_brk = new_brk;
+			}
 #endif
 		}
 	}
