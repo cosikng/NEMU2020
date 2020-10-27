31395f3f9ec235e2349bf1d07f8292244a52bffc
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-27 10:54:44 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:54:44 up 12:13,  1 user,  load average: 0.39, 0.46, 0.30 33ecfc5bf633ab2a24d8ff4c950986f81348c452

---
+++ nemu/src/cpu/exec/CMOVcc/cmove.h
@@ -0,0 +1,14 @@
+#ifndef __CMOVE_H__
+#define __CMOVE_H__
+#include "cpu/exec/helper.h"
+
+make_helper(cmove_r2rm_b);
+make_helper(cmove_rm2r_b);
+
+
+
+make_helper(cmove_r2rm_v);
+make_helper(cmove_rm2r_v);
+
+
+#endif
--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,5 +1,7 @@
 #include "prefix/prefix.h"
 
+#include "CMOVcc/cmove.h"
+
 #include "data-mov/mov.h"
 #include "data-mov/xchg.h"
 #include "data-mov/movsx.h"
