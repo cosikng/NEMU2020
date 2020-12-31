247fffc1243f3be45323dabd176b83351540fd94
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-01 00:56:02 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:56:02 up 17:17,  1 user,  load average: 0.18, 0.21, 0.13 9271059ea46856c8caa1272fc672409fb73fd304

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -23,11 +23,17 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len)
 		d1 = read_cache(addr, up - addr, &flag);
 		d2 = read_cache(up, addr + len - up, &flag);
 		if (d1 + (d2 << 16) != (dram_read(addr, len) & (~0u >> ((4 - len) << 3))))
-			panic("2read0x%x,expected0x%x\n",d1 + (d2 << 16),(dram_read(addr, len) & (~0u >> ((4 - len) << 3))));
+		{
+			printf("2read0x%x,expected0x%x\n", d1 + (d2 << 16), (dram_read(addr, len) & (~0u >> ((4 - len) << 3))));
+			panic("read\n");
+		}
 		return d1 + (d2 << 16);
 	}
 	if (read_cache(addr, len, &flag) != (dram_read(addr, len) & (~0u >> ((4 - len) << 3))))
-		panic("1read0x%x,expected0x%x\n",d1 + (d2 << 16),(dram_read(addr, len) & (~0u >> ((4 - len) << 3))));
+	{
+		printf("1read0x%x,expected0x%x\n", d1 + (d2 << 16), (dram_read(addr, len) & (~0u >> ((4 - len) << 3))));
+		panic("read\n");
+	}
 	return read_cache(addr, len, &flag);
 	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
