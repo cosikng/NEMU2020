19c954555c601b5fb72561d266ad75fd6c7e9e6f
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-03 23:02:52 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:02:52 up  2:51,  1 user,  load average: 0.02, 0.05, 0.07 9a998ceb5889d6d7a1e0e3561dba7f6e37d7324e

--- nemu/src/cpu/exec/data-mov/gdt.c
+++ nemu/src/cpu/exec/data-mov/gdt.c
@@ -5,9 +5,7 @@ make_helper(lgdt)
     decode_rm_l(eip + 1);
     uint32_t addr = op_src->addr;
     cpu.GDTR.limit = swaddr_read(addr, 2);
-    printf("0x%x\n", addr);
     cpu.GDTR.base_l = swaddr_read(addr + 2, 2);
-    printf("0x%x\n", addr);
     cpu.GDTR.base_h = swaddr_read(addr + 4, 2);
 
     return 5;
