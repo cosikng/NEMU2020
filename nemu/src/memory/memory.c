8ae6bc4eb0e4e3a3003bdb621f911d08c2255de0
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-05 01:25:00 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:25:00 up 1 day,  5:13,  1 user,  load average: 0.05, 0.29, 0.28 49bc8ec13e035c0fd5ce8acfa7b8364c9d3f3c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -102,7 +102,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data)
 		if (addr + len >= (addr & 0xfffff000) + 0x1000)
 		{
 			sublen = (addr & 0xfffff000) + 0x1000 - addr;
-			hwaddr_write(paddr, sublen, data & ((1 << sublen) - 1));
+			hwaddr_write(paddr, sublen, data & ((1 << sublen * 8) - 1));
 			pageitem = hwaddr_read((diritem & 0xfffff000) + page * 4 + 4, 4); //读取下一页
 			assert((pageitem & 1) == 1);
 			paddr = (pageitem & 0xfffff000);
