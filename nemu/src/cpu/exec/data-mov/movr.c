d8c5f3acfa2759a81009814ea48da099253e3b3a
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-04 17:31:48 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:31:48 up 21:19,  1 user,  load average: 0.17, 0.17, 0.17 126f19952e3a5e6d9ef139a88967977af279e657

--- nemu/src/cpu/exec/data-mov/movr.c
+++ nemu/src/cpu/exec/data-mov/movr.c
@@ -3,13 +3,21 @@
 make_helper(movs2r)
 {
     uint8_t reg = swaddr_read(eip + 1, 1, 1) & 7;
-    cpu.gpr[reg]._32 = cpu.CR0.val;
+    uint8_t s = (swaddr_read(eip + 1, 1, 1) >> 3) & 7;
+    if (s == 0)
+        cpu.gpr[reg]._32 = cpu.CR0.val;
+    else if (s == 3)
+        cpu.gpr[reg]._32 = cpu.CR3.val;
     return 2;
 }
 
 make_helper(movr2s)
 {
     uint8_t reg = swaddr_read(eip + 1, 1, 1) & 7;
-    cpu.CR0.val = cpu.gpr[reg]._32;
+    uint8_t s = (swaddr_read(eip + 1, 1, 1) >> 3) & 7;
+    if (s == 0)
+        cpu.CR0.val = cpu.gpr[reg]._32;
+    else if (s == 3)
+        cpu.CR3.val = cpu.gpr[reg]._32;
     return 2;
 }
