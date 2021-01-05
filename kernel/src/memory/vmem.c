3319d98d4aa37793bb13f8cd9d5967445268338a
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-05 15:59:39 +0800

    >  compile kernel 3019244336 abc Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:59:39 up  1:07,  1 user,  load average: 0.12, 0.23, 0.20 d665619cf4f3180fdfeb822bd680646638294ab6

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -5,6 +5,15 @@
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
 
+#ifdef IA32_PAGE
+#define KOFFSET 0xC0000000
+#else
+#define KOFFSET 0
+#endif
+
+#define va_to_pa(addr) ((void*)(((uint32_t)(addr)) - KOFFSET))
+#define pa_to_va(addr) ((void*)(((uint32_t)(addr)) + KOFFSET))
+
 static PTE videotable[(SCR_SIZE & ~PAGE_MASK) / PAGE_SIZE + 1] align_to_page;
 
 /* Use the function to get the start address of user page directory. */
@@ -22,7 +31,7 @@ void create_video_mapping()
 	PDE *updir = get_updir();
 	for (i = 0; i < (SCR_SIZE & ~(PT_SIZE - 1)) / PT_SIZE + 1; i++)
 	{
-		updir[i + (VMEM_ADDR >> 22)].val = (uint32_t)(videotable + i * PAGE_SIZE);
+		updir[i + (VMEM_ADDR >> 22)].val = (uint32_t)(va_to_pa(videotable) + i * PAGE_SIZE);
 		updir[i + (VMEM_ADDR >> 22)].present = 1;
 		for (j = 0; j < (SCR_SIZE & ~PAGE_MASK) / PAGE_SIZE + 1; j++)
 		{
