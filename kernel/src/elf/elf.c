cb9c5e0cb38f7ee06027500f9ffbcdb48dcc300e
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-05 14:49:29 +0800

    >  compile kernel 3019244336 abc Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:49:29 up 1 day,  6:07,  1 user,  load average: 0.27, 0.14, 0.14 a69cca3e8d85199150c750d65a3b9ad8690f3d6f

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -77,10 +77,10 @@ uint32_t loader()
 	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
 
 #ifdef HAS_DEVICE
-	create_video_mapping();
+	//create_video_mapping();
 #endif
 	write_cr3(get_ucr3());
 #endif
-
+	create_video_mapping();
 	return entry;
 }
