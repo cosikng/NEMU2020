b2e08350008610bff0270d469916ca35c8256b2d
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-11 00:44:25 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:44:25 up 37 min,  1 user,  load average: 0.14, 0.12, 0.17 bc2fa45cee2397359f852877edeaaed0fb5e52c1

--- nemu/src/cpu/exec/call/jmp.c
+++ nemu/src/cpu/exec/call/jmp.c
@@ -15,11 +15,16 @@
 /* for instruction encoding overloading */
 
 make_helper_v(jmp_si)
-    make_helper_v(jmp_rm)
+make_helper_v(jmp_rm)
 
-        make_helper(ljmp)
+make_helper(ljmp)
 {
+    uint32_t gdl, gdh;
     cpu.CS = swaddr_read(eip + 5, 2, 1) >> 3;
+    gdl = lnaddr_read((cpu.GDTR.base_h << 16) + cpu.GDTR.base_l + (cpu.CS >> 3) * 8, 4);
+    gdh = lnaddr_read((cpu.GDTR.base_h << 16) + cpu.GDTR.base_l + (cpu.CS >> 3) * 8 + 4, 4);
+    cpu.CSlimit = ((gdh & 0xf0000) << 12) + ((gdl & 0xffff) << 12);
+    cpu.CSbase = (gdl >> 16) + ((gdh & 0xff) << 16) + (gdh & 0xff000000);
     cpu.eip = swaddr_read(eip + 1, 4, 1) - 7;
     return 7;
 }
--- nemu/src/cpu/exec/data-mov/mov.c
+++ nemu/src/cpu/exec/data-mov/mov.c
@@ -22,7 +22,12 @@ make_helper_v(mov_a2moffs)
 make_helper_v(mov_moffs2a)
 make_helper(mov_r2s)
 {
+    uint32_t gdl, gdh;
     int data = swaddr_read(eip + 1, 1, 1); //CS
     cpu.Sreg[(data >> 3) & 7] = cpu.gpr[data & 7]._16;
+    gdl = lnaddr_read((cpu.GDTR.base_h << 16) + cpu.GDTR.base_l + (cpu.gpr[data & 7]._16 >> 3) * 8, 4);
+    gdh = lnaddr_read((cpu.GDTR.base_h << 16) + cpu.GDTR.base_l + (cpu.gpr[data & 7]._16 >> 3) * 8 + 4, 4);
+    cpu.Sregcache[(data >> 3) & 7].limit = ((gdh & 0xf0000) << 12) + ((gdl & 0xffff) << 12);
+    cpu.Sregcache[(data >> 3) & 7].base = (gdl >> 16) + ((gdh & 0xff) << 16) + (gdh & 0xff000000);
     return 2;
 }
