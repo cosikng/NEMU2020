2b95f772403e6d86b0d54dd25bfc4258eb3a9cf5
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-13 17:21:43 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:21:43 up 51 min,  1 user,  load average: 1.19, 0.90, 0.63 d5f701958a3502dd35a00de932794142807be866

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -95,14 +95,15 @@ uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg)
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
+	uint32_t lnaddr = 0;
 	if ((cpu.CR0.val & 1) == 1)
 	{
-		addr += cpu.Sregcache[sreg].base;
-		if (addr + len > cpu.Sregcache[sreg].limit)
+		lnaddr += cpu.Sregcache[sreg].base;
+		if (lnaddr + len > cpu.Sregcache[sreg].limit)
 		{
 			panic("segment out limit\n");
 		}
-		return lnaddr_read(addr + cpu.Sregcache[sreg].base, len);
+		return lnaddr_read(lnaddr, len);
 	}
 	return lnaddr_read(addr, len);
 }
@@ -112,14 +113,15 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data, uint8_t sreg)
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
+	uint32_t lnaddr = 0;
 	if ((cpu.CR0.val & 1) == 1)
 	{
-		addr += cpu.Sregcache[sreg].base;
-		if (addr + len > cpu.Sregcache[sreg].limit)
+		lnaddr += cpu.Sregcache[sreg].base;
+		if (lnaddr + len > cpu.Sregcache[sreg].limit)
 		{
 			panic("segment out limit\n");
 		}
-		lnaddr_write(addr + cpu.Sregcache[sreg].base, len, data);
+		lnaddr_write(lnaddr, len, data);
 		return;
 	}
 	lnaddr_write(addr, len, data);
