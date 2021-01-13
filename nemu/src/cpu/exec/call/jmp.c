336ee64a7b02bbe9884cda558fe71924f3ea6d32
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-13 17:41:14 +0800

    >  compile NEMU 3019244336 abc Linux ubuntu 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:41:14 up  1:11,  1 user,  load average: 0.38, 0.25, 0.35 278d739980c0bc5ef18b2a01f1a46ace6dba1685

--- nemu/src/cpu/exec/call/jmp.c
+++ nemu/src/cpu/exec/call/jmp.c
@@ -20,11 +20,13 @@ make_helper_v(jmp_rm)
 make_helper(ljmp)
 {
     uint32_t gdl, gdh;
-    //cpu.CS = swaddr_read(eip + 5, 2, 1) >> 3;
+    cpu.CS = swaddr_read(eip + 5, 2, 1) >> 3;
+    printf("1");
     gdl = lnaddr_read((cpu.GDTR.base_h << 16) + cpu.GDTR.base_l + (cpu.CS >> 3) * 8, 4);
     gdh = lnaddr_read((cpu.GDTR.base_h << 16) + cpu.GDTR.base_l + (cpu.CS >> 3) * 8 + 4, 4);
     cpu.CSlimit = ((gdh & 0xf0000) << 12) + ((gdl & 0xffff) << 12);
     cpu.CSbase = (gdl >> 16) + ((gdh & 0xff) << 16) + (gdh & 0xff000000);
-    //cpu.eip = swaddr_read(eip + 1, 4, 1) - 7;
+    cpu.eip = swaddr_read(eip + 1, 4, 1) - 7;
+    printf("1");
     return 7;
 }
